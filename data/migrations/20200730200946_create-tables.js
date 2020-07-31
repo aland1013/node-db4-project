d431f1ba92fb6b1f0a6b1775816089adc10490af
aland1013 <adam.land@gmail.com>
2020-07-30 20:29:48 -0400

    created and ran migration to create tables

---
+++ data/migrations/20200730200946_create-tables.js
@@ -0,0 +1,48 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('recipes', (tbl) => {
+      tbl.increments();
+      tbl.string('recipeName', 128).notNullable();
+    })
+    .createTable('ingredients', (tbl) => {
+      tbl.increments();
+      tbl.string('ingredientName', 128).notNullable();
+    })
+    .createTable('recipe_ingredients', (tbl) => {
+      tbl.float('quantity').notNullable();
+      tbl
+        .integer('recipeId')
+        .unsigned()
+        .notNullable()
+        .references('recipes.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl
+        .integer('ingredientId')
+        .unsigned()
+        .notNullable()
+        .references('ingredients.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl.primary(['recipeId', 'ingredientId']);
+    })
+    .createTable('instructions', (tbl) => {
+      tbl.increments();
+      tbl.string('step', 128).notNullable();
+      tbl
+        .integer('recipeId')
+        .unsigned()
+        .notNullable()
+        .references('recipes.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('instructions')
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
+};
